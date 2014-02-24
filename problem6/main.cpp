2c53803a0522a0412914f079e3596ed93d87bdba
Simon Racz <simonracz@gmail.com>
2014-02-25 00:44:56 +0100

    problem 6 solved.

---
+++ problem6/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include <prime_numbers.hpp>
+
+int main(int argc, char* argv[]) {
+
+	int limit = 100;
+	
+	long long ret = 0;
+	
+	for (int i=1; i<=limit; ++i) {
+		for (int j=1; j<=limit; ++j) {
+			ret += i * j;
+		}
+	}
+	
+	for (int i=1; i<=limit; ++i) {
+		ret -= i * i;
+	}
+	
+	std::cout << ret << "\n";
+}
+
